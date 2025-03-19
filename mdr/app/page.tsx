8ef4a526f0f3b56214e854932d01b5b154b19039
Jonathan Sheahan <jon.sheahan@gmail.com>
2025-03-19 22:03:55 +1100

    Update page.tsx

--- mdr/app/page.tsx
+++ mdr/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
       <div className="max-w-6xl mx-auto">
         <h1 className="text-4xl font-bold mb-8 text-[#24e4f2]">Macrodata Refinement</h1>
         <p className="text-[#24e4f2] mb-8">Data Engineers, welcome to the Macrodata Refinement challenge!
-          <br>Play as a human and use arrow keys to navigate the grid and find magic squares.</br>
-          <br>Or add your own custom strategies and see how your AI performs.</br>
+          
         </p>
         <MagicSquareVisualizer />
       </div>
